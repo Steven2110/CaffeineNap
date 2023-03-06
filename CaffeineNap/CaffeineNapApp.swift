1005ac56bc1f031ac0d3c5fefef5e211e6f6803f
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-03-06 16:42:33 +0700

    Initial Commit

---
+++ CaffeineNap/CaffeineNapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaffeineNapApp.swift
+//  CaffeineNap
+//
+//  Created by Steven Wijaya on 06.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct CaffeineNapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
