c72092ed9e9bb7852db10a5a1ff5aaf952a276f6
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-06-04 14:08:25 +0700

    Added UIImage extension to convert UIImage to CKAsset, Added Utilities folder and move all Extensions and AlertItem

---
+++ CaffeineNap/Utilities/Extensions/UIImageExt.swift
@@ -0,0 +1,37 @@
+//
+//  UIImageExt.swift
+//  CaffeineNap
+//
+//  Created by Steven Wijaya on 04.06.2023.
+//
+
+import UIKit
+import CloudKit
+
+extension UIImage {
+    
+    func convertToCKAsset() -> CKAsset? {
+        
+        // Get our app's base document directory url
+        guard let urlPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            return nil
+        }
+        
+        // Append unique identifier for profile image
+        let fileUrl = urlPath.appendingPathComponent("selectedAvatarImage")
+        
+        // Write the image data to the location the address points to
+        guard let imageData = jpegData(compressionQuality: 0.25) else { return nil }
+        
+        //Create our CKAsset with that fileURL
+        do {
+            try imageData.write(to: fileUrl)
+            
+            return CKAsset(fileURL: fileUrl)
+        } catch {
+            return nil
+        }
+        
+    }
+    
+}
